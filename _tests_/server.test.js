8d243c99ee2175a8060658e2c4a183a1204f1256
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-06-13 20:57:36 -0700

    lab1

---
+++ _tests_/server.test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const supertest = require('supertest');
+const server = require('../src/server');
+const request = supertest(server.app);
+
+describe('Server Tests', ()=>{
+  describe('GET request', ()=>{
+    it('responds with status 200 /route', async()=>{
+      const response = await request.get('/');
+      expect(response.status).toEqual(200);
+    });
+    test('api route works', async()=>{
+      const response = await request.get('/api');
+      expect(response.status).toEqual(200);
+      console.log(response.text,response.body);
+      expect(response.body).toEqual({"name":"John", "age":30, "car":null})
+    });
+  });
+});
